5716db87fc924392d41521377ce994a380408bf8
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-03-10 16:17:13 +0100

    S10

--- S10/happy-server.py
+++ S10/happy-server.py
@@ -1,3 +1,29 @@
+import socket
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "212.128.255.88"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
 while True:
     # -- Waits for a client to connect
     print("Waiting for Clients to connect")
@@ -39,3 +65,4 @@ while True:
 
         # -- Close the data socket
         cs.close()
+
