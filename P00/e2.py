8e2d8801dd4e0066c09c1864fb85edf05694cc7f
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-03-11 18:07:11 +0100

    P00

--- P00/e2.py
+++ P00/e2.py
@@ -3,4 +3,4 @@ import Seq0
 U5 = "../S04/sequences/U5.txt"
 seq = Seq0.base_printer(U5)
 
-print("DNA FILE: U5.txt \nThe first 20 bases are:", seq[0:20])
+print("DNA FILE: U5.txt \nThe first 20 bases are:\n"+seq[0:20])
