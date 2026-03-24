14003ba3ade6c29b6df7adb80ac16892668a9453
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-03-24 04:29:23 +0100

    P01

--- P00/e5.py
+++ P00/e5.py
@@ -2,14 +2,9 @@ import Seq0
 
 seqs = ["U5", "ADA", "FRAT1", "FXN"]
 
-print("-----| Exercise 4 |------")
+print("-----| Exercise 5 |------")
 
 for seq in range(len(seqs)):
-    print(seqs[seq])
     desired_sequence = f"../S04/sequences/{seqs[seq]}.txt"
     clean_des_seq = Seq0.base_printer(desired_sequence)
-    for base, number in Seq0.seq_count_base(clean_des_seq).items():
-        print(f"{base}: {number}")
-
-
-
+    print(f"Gene {seqs[seq]}:",Seq0.seq_count_base(clean_des_seq))
--- S04/header.py
+++ S04/header.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
 FILENAME = "./sequences/RNU6_269P.txt"
+from pathlib import Path
+
+FILENAME = "./sequences/RNU6_269P.txt"
 file_contents = Path(FILENAME).read_text()
 
 split_contents = file_contents.split("\n")
