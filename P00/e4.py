e8372230ecb505b62301fbef7007381ddba1528a
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-03-24 04:31:04 +0100

    P00

--- P00/e4.py
+++ P00/e4.py
@@ -10,6 +10,3 @@ for seq in range(len(seqs)):
     clean_des_seq = Seq0.base_printer(desired_sequence)
     for base, number in Seq0.seq_count_base(clean_des_seq).items():
         print(f"{base}: {number}")
-
-
-
--- P00/e5.py
+++ P00/e5.py
@@ -8,3 +8,4 @@ for seq in range(len(seqs)):
     desired_sequence = f"../S04/sequences/{seqs[seq]}.txt"
     clean_des_seq = Seq0.base_printer(desired_sequence)
     print(f"Gene {seqs[seq]}:",Seq0.seq_count_base(clean_des_seq))
+
