af11bc03a1bf7d6809899b8a989714a7d87aa9da
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-14 17:00:25 +0200

    P01

--- P01/e4.py
+++ P01/e4.py
@@ -1,9 +1,9 @@
 from Seq1 import Seq
-print("-----| Practice 1 Exercise 4 |------")
-seq = Seq("TATAC")
-seq2 = Seq()
-seq3 =Seq("Invalid")
 
-print(f"Sequence 1: {seq} (Length: {seq.length()}) ")
-print(f"Sequence 2: {seq2} (Length: {seq2.length()}) ")
-print(f"Sequence 3: {seq3} (Length: {seq3.length()}) ")
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: (Length: {s1.len()}) {s1.str_bases}")
+print(f"Sequence 2: (Length: {s2.len()}) {s2.str_bases}")
+print(f"Sequence 3: (Length: {s3.len()}) {s3.str_bases}")
