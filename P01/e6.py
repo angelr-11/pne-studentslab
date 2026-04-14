5c670aff2b5feaa9c0821d1ec08f277ae77858d5
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-14 17:51:24 +0200

    P01

--- P01/e6.py
+++ P01/e6.py
@@ -1,14 +1,12 @@
 from Seq1 import Seq
-print("-----| Practice 1 Exercise 6 |------")
-seq = Seq("TATAC")
-seq2 = Seq()
-seq3 =Seq("Invalid")
-print(f"Sequence 1: {seq} (Length: {seq.length()}) ")
-print(f"Bases:, {seq.count()}")
-print()
-print(f"Sequence 1: {seq2} (Length: {seq2.length()}) ")
-print(f"Bases:, {seq2.count()}")
-print()
-print(f"Sequence 1: {seq3} (Length: {seq3.length()}) ")
-print(f"Bases:, {seq3.count()}")
-print()
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+sequences = [s1, s2, s3]
+
+for i, s in enumerate(sequences, start=1):
+    print(f"Sequence {i}: (Length: {s.len()}) {s.str_bases}")
+    d = {"A": s.scb("A"), "C": s.scb("C"), "T": s.scb("T"), "G": s.scb("G")}
+    print(d)
