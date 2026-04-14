226d54a3e116f76cad64e7be386e207e26201423
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-14 16:21:53 +0200

    P01

--- P01/e1.py
+++ P01/e1.py
@@ -1,4 +1,5 @@
 from Seq1 import Seq
-print("-----| Practice 1 Exercise 1 |------")
-seq = Seq("ACTGA")
-print(f"Sequence 1: (Length: {seq.length()}) {str(seq)}")
+
+print("-----| Practice 1, Exercise 1 |------")
+seq1 = Seq("ACTGA")
+print(f"Sequence 1: (Length: {seq1.len()}) {seq1.str_bases}")
