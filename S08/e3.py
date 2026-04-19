37932b97f6b049013adfd089d33e10a957fe6612
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-19 17:23:58 +0200

    S08

--- S08/e3.py
+++ S08/e3.py
@@ -2,7 +2,7 @@ import socket
 
 # SERVER IP, PORT
 PORT = 8081
-IP = "192.168.124.179" # depends on the computer the server is running
+IP = "127.0.0.1" # depends on the computer the server is running
 
 while True:
       # -- Ask the user for the message
