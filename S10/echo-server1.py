4ee00a7e5cf7cbc64200754d90f0b359efa2fb81
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-21 23:49:04 +0200

    S10

--- S10/echo-server1.py
+++ S10/echo-server1.py
@@ -1,65 +1,32 @@
 import socket
+import termcolor
 
-# -- Step 1: create the socket
-ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# -- Optional: This is for avoiding the problem of Port already in use
-ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Configure the Server's IP and PORT
 PORT = 8080
-IP = "212.128.255.88"
+IP = "127.0.0.1"
 
-# -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# -- Optional: This is for avoiding the problem of Port already in use
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
-
-# -- Step 3: Configure the socket for listening
 ls.listen()
 
 print("The server is configured!")
 
 while True:
-    # -- Waits for a client to connect
-    print("Waiting for Clients to connect")
-
+    print("Waiting for connection...")
     try:
         (cs, client_ip_port) = ls.accept()
-
-    # -- Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
-
-        # -- Close the listenning socket
         ls.close()
-
-        # -- Exit!
         exit()
-
-    # -- Execute this part if there are no errors
     else:
-
         print("A client has connected to the server!")
 
-        # -- Read the message from the client
-        # -- The received message is in raw bytes
         msg_raw = cs.recv(2048)
+        msg = termcolor.colored(msg_raw.decode(), "green")
+        print(f"Message received: {msg}")
 
-        # -- We decode it for converting it
-        # -- into a human-redeable string
-        msg = msg_raw.decode()
-
-        # -- Send a response message to the client
-        response = f"ECHO {msg}\n"
-
-        # -- The message has to be encoded into bytes
+        response = f"Echo: {msg}"
         cs.send(response.encode())
-
-        # -- Close the data socket
         cs.close()
-
