b08661552f5b5f6b6e9bb60510d94085fd1095aa
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-22 18:32:11 +0200

    P03

--- P03/SeqServer.py
+++ P03/SeqServer.py
@@ -1,137 +1,58 @@
 import socket
+import termcolor
+from P01 import Seq1
 
-from P00.Seq0 import seq_complement
-from Seq1 import*
 
-
-# -- Step 1: create the socket
-ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# -- Optional: This is for avoiding the problem of Port already in use
-ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Configure the Server's IP and PORT
 PORT = 8080
-IP = "212.128.255.86"
+IP = "127.0.0.1"
 
-# -- Step 1: create the socket
 ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# -- Optional: This is for avoiding the problem of Port already in use
 ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# -- Step 2: Bind the socket to server's IP and PORT
 ls.bind((IP, PORT))
-
-# -- Step 3: Configure the socket for listening
 ls.listen()
 
-print("The server is configured!")
-
 while True:
-    # -- Waits for a client to connect
-    print("Waiting for Clients to connect")
-
+    print("Waiting for connection...")
     try:
         (cs, client_ip_port) = ls.accept()
-
-    # -- Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
-
-        # -- Close the listenning socket
         ls.close()
-
-        # -- Exit!
         exit()
-
-    # -- Execute this part if there are no errors
     else:
-
-        print("A client has connected to the server!")
-
-        # -- Read the message from the client
-        # -- The received message is in raw bytes
         msg_raw = cs.recv(2048)
+        command = msg_raw.decode().strip()
+        parts = command.split()
+        print(f"Incoming command: {termcolor.colored(command,"green")}")
+        #e1
+        if command == "PING":
+            rs = "OK!"
+        elif len(parts) == 2:
+            #e2
+            if parts[0] == "GET" and parts[1].isdigit() and 0 <= int(parts[1]) <= 4:
+                n = int(parts[1])
+                seqs = {0: "AAA", 1:"TTT", 2:"GGG", 3:"CCC", 4:"ATC"}
+                rs = seqs.get(n,"error")
+            #e3
+            elif parts[0] == "INFO":
+                eseq = Seq1.Seq(parts[1])
+                if eseq.str_bases == "ERROR" or eseq.str_bases == "NULL":
+                    rs = "NON VALID SEQUENCE"
+                else:
+                    rs = f"""Sequence: {eseq.str_bases}
+                    Total length: {eseq.len()}
+                    A: {eseq.scb("A")} ({round((eseq.scb("A")/eseq.len())*100,2)}%)
+                    C: {eseq.scb("C")} ({round((eseq.scb("C")/eseq.len())*100,2)}%)
+                    G: {eseq.scb("G")} ({round((eseq.scb("G")/eseq.len())*100,2)}%)
+                    T: {eseq.scb("T")} ({round((eseq.scb("T")/eseq.len())*100,2)}%)\n"""
+                    print(rs)
 
-        # -- We decode it for converting it
-        # -- into a human-redeable string
-        msg = msg_raw.decode()
-        x = msg.split(" ")
-        if x[0] == "PING":
-            print(f"PING Command!")
-            response = "OK\n"
+        else:
+            rs = "error"
 
-        if x[0] == "GET":
-            if x[1] == "0":
-                response = "AAAGGGTTTCCCAAAAA"
-            if x[1] == "1":
-                response = "AAAGGGTTTCCCAGGGG"
-            if x[1] == "2":
-                response = "AAAGGGTTTCCCAAGGG"
-            if x[1] == "3":
-                response = "AAAGGGTTTCCCAAAGG"
-            if x[1] == "4":
-                response = "AAAGGGTTTCCCAAAAG"
 
-        if x[0] == "INFO":
-            seq1 = x[1]
-            s = Seq(seq1)
-            dict_result = s.count_bases2()
 
-            response = ""
-            response += f"Sequence: {seq1}\n"
-            response += f"Total length: {len(seq1)}\n"
-            for base in ["A", "C", "G", "T"]:
-                response += f"{base}: {dict_result[base]['times']} ({dict_result[base]['percentage']}%)\n"
-
-        if x[0] == "COMP":
-            seq1 = x[1]
-            print(seq1)
-            s = Seq(seq1)
-            result = s.seq_complement()
-            response = result
-
-        if x[0] == "REV":
-            seq1 = x[1]
-            print(seq1)
-            s = Seq(seq1)
-            result = s.reverse()
-            response = result
-
-        if x[0] == "GENE":
-            if x[1] == "U5":
-                U5 = "../sequences/U5.txt"
-                s1 = Seq()
-                s11 = s1.seq_read_fasta(U5)
-
-                response = s11
-            if x[1] == "ADA":
-                ADA = "../sequences/ADA.txt"
-                s1 = Seq()
-                s11 = s1.seq_read_fasta(ADA)
-
-                response = s11
-            if x[1] == "FRAT1":
-                FRAT1 = "../sequences/FRAT1.txt"
-                s1 = Seq()
-                s11 = s1.seq_read_fasta(FRAT1)
-
-                response = s11
-            if x[1] == "FXN":
-                FXN = "../sequences/FXN.txt"
-                s1 = Seq()
-                s11 = s1.seq_read_fasta(FXN)
-
-                response = s11
-            if x[1] == "RNU6_269P":
-                RNU6_269P = "../sequences/RNU6_269P.txt"
-                s1 = Seq()
-                s11 = s1.seq_read_fasta(RNU6_269P)
-
-                response = s11
-
-        cs.send(response.encode())
-
-        # -- Close the data socket
+        cs.send(f"{rs}".encode())
         cs.close()
+
