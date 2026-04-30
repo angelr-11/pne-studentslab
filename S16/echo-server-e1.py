bd5f7e13ac0b4290e990f708111c643c41b0fd30
angelr <ar.arrieta.2025@alumnos.urjc.es>
2026-04-30 18:45:26 +0200

    S16

---
+++ S16/echo-server-e1.py
@@ -0,0 +1,45 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+PORT = 8080
+
+socketserver.TCPServer.allow_reuse_address = True
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        termcolor.cprint(self.requestline, 'green')
+        command = self.requestline.split(" ")[1]
+        if command in ["/","/echo"]:
+            contents = Path('html/form-e1.html').read_text()
+        elif command.split("?")[0] == "/echo?":
+            contents = Path(f"/echo?message=)
+        else:
+            contents = Path("html/error.html").read_text()
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+
+        self.end_headers()
+
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
